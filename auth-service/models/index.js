cecbd9e1bd9797b5bbd85530889b97f50e91255d
CamNhungJB <22521057@gm.uit.edu.vn>
2024-06-19 22:46:19 +0700

    delete code main

--- auth-service/models/index.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require('../config/db');
-const User = require('./user');
-
-const initDb = async () => {
-  await sequelize.sync({ force: true });
-  console.log("All models were synchronized successfully.");
-};
-
-module.exports = { User, initDb };
